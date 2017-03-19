31bf4a27ca4e45c95d1d80426969f262179a0222
pterodactylogan <loganswan37@gmail.com>
2017-03-18 22:16:15 -0400

    imageView added

---
+++ Test/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Test
+//
+//  Created by Logan Swanson on 3/18/17.
+//  Copyright © 2017 Logan Swanson. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
